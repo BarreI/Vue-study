abbfb0f95ca3ab86b7f380a278328f4f58ca0c7d
AntiMelee <tmskiti@outlook.jp>
2021-02-18 16:14:32 +0900

    v-for簡単な練習

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-var app = new Vue({ 
-  el: '#app',
+var app = new Vue({
+  el: "#app",
   data: {
-      message: 'Hello Vue!'
+    message: "実験ちゅうううううううううううううううううう"
   }
-});
+})
+
+var tabley = new Vue({
+  el: '#tabley',
+  data: {
+    items: [//ここにメンバーリストをぶちこむ
+      { message: '太郎' }, 
+      { message: '次郎' },
+      { message: '三郎' }
+    ]
+  }
+})
+
+var tablex = new Vue({
+  el:"#tablex",
+  data: {
+    items: [
+      { message: "1"},
+      { message: "2"},
+      { message: "3"}
+    ]
+  }
+})
