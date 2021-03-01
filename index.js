5c07a5ba072726bd537fa3b0c3cc8e6a1bb07ecb
AntiMelee <tmskiti@outlook.jp>
2021-03-01 10:45:52 +0900

    月単位のカレンダーを実装

--- index.js
+++ index.js
@@ -1,42 +1,28 @@
-const { UserInfo } = require("git");
-
-var memberList = new Vue({
-  el: 'memberList',
-  data: {
-    list : [
-      {systemid:111,name:'太郎'},
-      {systemid:222,name:'次郎'},
-      {systemid:333,name:'ハナコ'}
-    ]
-  }
-})
-
-var shiftList = new Vue({
-  el: 'shiftList',
-  data: {
-    list :[
-      {systemid:111,date:"13/32",start:"9:00",end:"12:00"},
-      {systemid:222,date:"13/32",start:"10:00",end:"14:00"},
-      {systemid:333,date:"13/32",start:"12:00",end:"18:00"}
-    ]
-  }
-})
-
-//この関数でどの幅描写するかが決定する
-function timeline(starttime,endtime){ //timeは0:00から何分か
-  let start = startime / (24 * 60);
-  let end = endtime / (24 * 60);
-  let timearray = [start, end];
-
-  return timearray;
-} //帰ってきたデータとユーザーをくっつけru
-
-
-//縦どこに描写するか
-function jobid(list){
-  list.sort()
-}
-
-
-//TODO 時間を百聞率に直す計算式
-//TODO 得た情報を元に%で描画
+const app = Vue.createapp({
+  data(){
+    return {
+      start_month: '2021-1',
+      end_month: '2022-01',
+      block_size: 30,
+      block_number: 0,
+    }
+  },
+  methods:{
+    getDays(year, month, block_number) {
+      const dayOfWeek = ['日', '月', '火', '水', '木', '金', '土'];
+      let days = [];
+      let date = moment(`${year}-${month}-01`);
+      let num = date.daysInMonth();
+      for (let i = 0; i < num; i++) {
+        days.push({
+          day: date.date(),
+          dayOfWeek: dayOfWeek[date.day()],
+          block_number
+        })
+        date.add(1, 'day');
+        block_number++;
+      }
+      return days;
+    },     
+  },
+}).mount('#app')
